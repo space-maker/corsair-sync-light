258bc90b58715f44f9e2c77125bf9ba0e6a86699
jonathan boaknin <jonathan.boaknin@hotmail.com>
2021-03-25 22:53:56 +0100

    [Update] verbose threading

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from cuesdk import CorsairLedId, CueSdk
 import keyboard
 import synclight
 import json
-from time import perf_counter
+from time import perf_counter, sleep
 from os import system
+import sys
+import threading
+import signal
+
 
 class SyncLightCorsairHandler:
     def __init__(self):
         self._sdk = CueSdk()
         self._config_keys = None
+        self._fps = 0
         
     def init(self):
         if not self._sdk.connect():
@@ -21,15 +26,26 @@ class SyncLightCorsairHandler:
     def run(self, ambilight_mode, keyboard_model, keyboard_mode):
         keysmap = list(map(self._map_keys, self._config_keys[keyboard_model]["keys"][keyboard_mode]))
         sync = synclight.SyncLightCorsair(keyboard.Keyboard(self._sdk, keysmap))
-        # sync.runtest()
+        verbose_threading = threading.Thread(target=self._verbose_threading)
+        verbose_threading.start()
 
-        while True:
-            begin = perf_counter()
-            sync.ambilight()
+        try:
+            while True:
+                begin = perf_counter()
+                sync.ambilight()
+                self._fps = 1.0 / (perf_counter() - begin)
+        except (KeyboardInterrupt, SystemExit):
+            self._fps = -1
+            sleep(1)
+            sys.quit(1)
 
-            system("cls")
-            print(1 / (perf_counter() - begin), "fps")
-
+    def _verbose_threading(self):
+        clear = lambda: system('cls')
+        while self._fps >= 0:
+            print(self._fps, "fps")
+            sleep(1)
+            clear()
+        
     def _map_keys(self, row):
         return list(map(lambda key: getattr(CorsairLedId, "K_" + key), row))
 
--- synclight.py
+++ synclight.py
@@ -9,23 +9,24 @@ class Screenshot:
         self.size_y = None
 
     def take_screen(self):
-        self.img = ImageGrab.grab()
+        self.img = ImageGrab.grab(include_layered_windows=True)
         self.size_x, self.size_y = self.img.size
 
-    def mean_frequencies_color(self, max_row, max_column, current_row, current_column):
+    def mean_frequencies_color(self, max_row, max_column, current_row, current_column, wordload=100):
         r, g, b = 0, 0, 0
     
-        sample_x = 10
-        sample_y = 10
+        sample_x = wordload
+        sample_y = wordload
         
-        n=0
+        n = 0
         for x in range(int(current_column * self.size_x / max_column), int((current_column + 1) * self.size_x / max_column), sample_x):
             for y in range(int(current_row * self.size_y / max_row), int((current_row + 1) * self.size_y / max_row), sample_y):
                 rgb = self.img.getpixel((x, y))
                 r += rgb[0]
                 g += rgb[1]
                 b += rgb[2]
-                n+=1
+                
+                n += 1
 
         r //= n
         g //= n
@@ -37,7 +38,7 @@ class SyncLightCorsair:
     def __init__(self, keyboard):
         self._keyboard = keyboard
 
-    def ambilight(self):
+    def ambilight(self, mode="mean_frequencies_color"):
         sc = Screenshot()
         sc.take_screen()
 
@@ -46,7 +47,7 @@ class SyncLightCorsair:
 
         for row, max_column in self._keyboard:
             for current_column in range(max_column):
-                rgb_color = sc.mean_frequencies_color(max_row, max_column, current_row, current_column)
+                rgb_color = getattr(sc, mode)(max_row, max_column, current_row, current_column)
                 row(current_column, rgb_color)
 
             current_row += 1
@@ -57,6 +58,6 @@ class SyncLightCorsair:
     def runtest(self):
         for row, columns in self._keyboard:
             for column in range(columns):
-                row(column, (255, 255, 255))
+                row(column, (255, 0, 255))
                 time.sleep(0.01)
                 self._keyboard.apply_colors()
